0f744eac4c6120c17d7898df2198ea40b83f7b4f
Huakun Shen <huakun.shen@mail.utoronto.ca>
2018-12-18 14:22:42 -0500

    added year, semester, sign up acitivity and their layout, not implemented yet

---
+++ app/src/main/java/project/gpa_calculator/year_activity.java
@@ -0,0 +1,13 @@
+package project.gpa_calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class year_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_year_activity);
+    }
+}

d5a1656b8a6f094d3e3e39d59ec77ffdce3258f7
Huakun Shen <huakun.shen@mail.utoronto.ca>
2018-12-19 22:45:46 +0800

    Added other activities and their layouts. Activities are about displaying and adding year, semester, course. Not Implemented yet.

---
+++ app/src/main/java/project/gpa_calculator/add_course.java
@@ -0,0 +1,26 @@
+package project.gpa_calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class add_course extends AppCompatActivity {
+    private EditText course_name_EditText;
+    private EditText course_name_EditText;
+    private EditText course_name_EditText;
+    private EditText course_name_EditText;
+    private EditText course_name_EditText;
+    private EditText course_name_EditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_course);
+        setupEditText();
+    }
+
+    private void setupEditText() {
+
+    }
+}
--- app/src/main/java/project/gpa_calculator/year_activity.java
+++ app/src/main/java/project/gpa_calculator/year_activity.java
@@ -8,6 +8,6 @@ public class year_activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_year_activity);
+        setContentView(R.layout.activity_year);
     }
 }

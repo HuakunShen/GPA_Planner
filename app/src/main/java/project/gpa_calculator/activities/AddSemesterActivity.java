64cd3164fa626d0d1b1d0669c9d2d03ceff4ff2d
Huakun Shen <huakun.shen@mail.utoronto.ca>
2018-12-22 19:56:23 -0600

    added a plus icon to SemesterActivity and many renaming

---
+++ app/src/main/java/project/gpa_calculator/activities/AddSemesterActivity.java
@@ -0,0 +1,15 @@
+package project.gpa_calculator.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import project.gpa_calculator.R;
+
+public class SemesterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_semester);
+    }
+}
--- app/src/main/java/project/gpa_calculator/activities/SemesterActivity.java
+++ app/src/main/java/project/gpa_calculator/activities/SemesterActivity.java
@@ -1,15 +1,31 @@
 package project.gpa_calculator.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import project.gpa_calculator.R;
 
-public class SemesterActivity extends AppCompatActivity {
+public class AddSemesterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_semester);
+        setContentView(R.layout.activity_add_semester);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
+
 }

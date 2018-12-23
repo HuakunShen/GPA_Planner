05ac6fbae28cd597fab280fb7414d131421df24a
Huakun Shen <huakun.shen@mail.utoronto.ca>
2018-12-22 18:53:59 -0600

    added a button in Main Activity to navigate to Year activity

--- app/src/main/java/project/gpa_calculator/activities/MainActivity.java
+++ app/src/main/java/project/gpa_calculator/activities/MainActivity.java
@@ -1,5 +1,6 @@
 package project.gpa_calculator.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import project.gpa_calculator.R;
 
@@ -42,7 +44,20 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        setupButton();
     }
+
+    private void setupButton() {
+        Button local_user_button = findViewById(R.id.local_user_B);
+        local_user_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplication(), year_activity.class));
+            }
+        });
+    }
+
 
     @Override
     public void onBackPressed() {

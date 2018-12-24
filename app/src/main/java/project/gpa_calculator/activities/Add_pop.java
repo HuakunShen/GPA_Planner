c1c7f1fc0dfa12abdd5fe19e1426a017045c0fbf
Jerry <emolangshen@gmail.com>
2018-12-24 09:27:42 -0500

    pop up window created for YearActivity

--- app/src/main/java/project/gpa_calculator/activities/Add_pop.java
+++ app/src/main/java/project/gpa_calculator/activities/Add_pop.java
@@ -1,15 +1,23 @@
 package project.gpa_calculator.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import project.gpa_calculator.R;
 
 public class Add_pop extends Activity {
 
+    private Button done,cancel;
+    private String year;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -29,5 +37,35 @@ public class Add_pop extends Activity {
         params.y = -20;
 
         getWindow().setAttributes(params);
+        takeInput();
+    }
+
+    protected  void takeInput(){
+        done = (Button)findViewById(R.id.done);
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText out = (EditText)findViewById(R.id.years);
+                year = out.getText().toString();
+                if(!year.equals("")){
+                    Intent intent = new Intent();
+                    intent.putExtra("editTextValue", year);
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+
+            }
+        });
+
+        cancel = (Button)findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/project/gpa_calculator/activities/YearActivity.java
+++ app/src/main/java/project/gpa_calculator/activities/YearActivity.java
@@ -10,26 +10,50 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import project.gpa_calculator.R;
 
 public class YearActivity extends AppCompatActivity {
     private Button add_course;
+    private ScrollView scrollView;
+    private LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_year);
+
 
         add_course = (Button)findViewById(R.id.add_year_button);
         add_course.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(), Add_pop.class);
-                startActivity(i);
+                startActivityForResult(i,1);
                 add_course.setTextColor(Color.RED);
 
             }
         });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if(resultCode == RESULT_OK) {
+                String strEditText = data.getStringExtra("editTextValue");
+                Button new_year = new Button(this);
+                scrollView = (ScrollView) findViewById(R.id.scroll);
+                new_year.setText(strEditText);
+
+                linearLayout = (LinearLayout)findViewById(R.id.scrollLayout);
+                //add_course.setText(strEditText);
+                // Create a LinearLayout element
+                linearLayout.addView(new_year);
+
+
+            }
+        }
     }
 
 

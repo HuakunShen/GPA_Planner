a0fd651b3e399e8dea86b72b8cddc46b2458d2f4
Jerry <emolangshen@gmail.com>
2018-12-27 21:38:21 -0500

    update GPA view with weird padding

--- app/src/main/java/project/gpa_calculator/Adapter/RecyclerViewAdapter.java
+++ app/src/main/java/project/gpa_calculator/Adapter/RecyclerViewAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +17,14 @@ import project.gpa_calculator.R;
 import project.gpa_calculator.Util.ActivityController;
 import project.gpa_calculator.Util.AddDialog;
 import project.gpa_calculator.activities.GPA_setter.GPA_setter_Activity;
+import project.gpa_calculator.activities.GPA_setter.GPA_setter_Controller;
 import project.gpa_calculator.activities.course.CourseActivity;
 import project.gpa_calculator.activities.course.CourseActivityController;
 import project.gpa_calculator.activities.event.EventActivity;
 import project.gpa_calculator.activities.semester.SemesterActivity;
 import project.gpa_calculator.activities.semester.SemesterActivityController;
 import project.gpa_calculator.activities.year.YearActivity;
+import project.gpa_calculator.models.GPAListItem;
 import project.gpa_calculator.models.ListItem;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
@@ -41,17 +44,38 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     @NonNull
     @Override
     public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.list_row, parent, false);
-        return new ViewHolder(view);
+        View view;
+        //TODO possible change to switch?
+        if(controller instanceof GPA_setter_Controller){
+             view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.gpa_row, parent, false);
+             return new GPAViewHolder(view);
+        }else{
+             view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.list_row, parent, false);
+             return new YearViewHolder(view);
+        }
+
+
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder viewHolder, int position) {
         ListItem item = list_items.get(position);
-        viewHolder.name.setText(item.getName());
-        viewHolder.description.setText(item.getDescription());
-        viewHolder.gpa.setText(item.getGpa());
+        if(controller instanceof GPA_setter_Controller){
+            GPAListItem cur_item = (GPAListItem)item;
+            GPAViewHolder cur_view = (GPAViewHolder) viewHolder;
+            cur_view.low.setHint(Integer.toString(cur_item.getLow()));
+            cur_view.high.setHint(Integer.toString(cur_item.getHigh()));
+            cur_view.gpa_point.setHint(Double.toString(cur_item.getGPA()));
+            cur_view.gpa_grade.setHint(cur_item.getGPA_mark());
+        }
+        else {
+            YearViewHolder cur_view = (YearViewHolder)viewHolder;
+            cur_view.name.setText(item.getName());
+            cur_view.description.setText(item.getDescription());
+            cur_view.gpa.setText(item.getGpa());
+        }
 
     }
 
@@ -73,13 +97,17 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     public Context getContext() {
         return context;
     }
-
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    public abstract class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+    public class YearViewHolder extends ViewHolder implements View.OnClickListener {
         private TextView name;
         private TextView description;
         private TextView gpa;
 
-        private ViewHolder(@NonNull View itemView) {
+        private YearViewHolder(@NonNull View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
             name = itemView.findViewById(R.id.title);
@@ -110,14 +138,37 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
                 // Here item.getName() returns the course code instead of name
                 intent.putExtra("course_code", item.getName());
                 context.startActivity(intent);
-            }else if (context instanceof GPA_setter_Activity) {
-
             }
 
 
-
-
             Toast.makeText(context, item.getName(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public class GPAViewHolder extends ViewHolder implements View.OnClickListener {
+        private TextView low,high,gpa_grade,gpa_point;
+
+        private GPAViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+            low = itemView.findViewById(R.id.low);
+            high = itemView.findViewById(R.id.high);
+            gpa_grade = itemView.findViewById(R.id.GPA_grade);
+            gpa_point = itemView.findViewById(R.id.GPA_point);
+
+            gpa_point.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+
+            low.setHint("lower bound");
+            low.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+            high.setHint("upper bound");
+            high.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+
+        }
+        @Override
+        public void onClick(View v) {
+
         }
     }
 

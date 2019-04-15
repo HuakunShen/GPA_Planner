f5cc079f5c28d4b1ed14c27f6217339f3213d8cf
Huakun Shen <shenhuakun@outlook.com>
2019-04-14 22:41:07 -0400

    update recyclerview (swipe to delete) so that it can be swiped back, only updated for event activity so far.

--- app/src/main/java/project/gpa_calculator/activities/event/EventActivityRecyclerViewAdapter.java
+++ app/src/main/java/project/gpa_calculator/activities/event/EventActivityRecyclerViewAdapter.java
@@ -6,8 +6,12 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.daimajia.swipe.SwipeLayout;
+
 import java.util.List;
 import project.gpa_calculator.R;
 import project.gpa_calculator.models.Event;
@@ -34,12 +38,98 @@ public class EventActivityRecyclerViewAdapter extends RecyclerView.Adapter<Event
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
-        Event event = this.list_items.get(position);
+        final Event event = this.list_items.get(position);
         viewHolder.name.setText(event.getEvent_name());
         viewHolder.weight.setText(("Weight: " + String.valueOf(event.getEvent_weight())));
         viewHolder.score.setText(("Score: " + String.valueOf(event.getEvent_score())));
         viewHolder.status.setText(("Status: " + (event.isDone() ? "Done" : "Not Done")));
         viewHolder.target.setText(("Target: " + String.valueOf(event.getTarget())));
+
+
+        viewHolder.swipeLayout.setShowMode(SwipeLayout.ShowMode.PullOut);
+        //dari kiri
+        viewHolder.swipeLayout.addDrag(SwipeLayout.DragEdge.Left, viewHolder.swipeLayout.findViewById(R.id.bottom_wrapper1));
+
+        //dari kanan
+        viewHolder.swipeLayout.addDrag(SwipeLayout.DragEdge.Right, viewHolder.swipeLayout.findViewById(R.id.bottom_wraper));
+
+//        viewHolder.swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+//            @Override
+//            public void onStartOpen(SwipeLayout layout) {
+//
+//            }
+//
+//            @Override
+//            public void onOpen(SwipeLayout layout) {
+//
+//            }
+//
+//            @Override
+//            public void onStartClose(SwipeLayout layout) {
+//
+//            }
+//
+//            @Override
+//            public void onClose(SwipeLayout layout) {
+//
+//            }
+//
+//            @Override
+//            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+//
+//            }
+//
+//            @Override
+//            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+//
+//            }
+//        });
+
+
+
+        viewHolder.swipeLayout.getSurfaceView().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, " Click : " + event.getEvent_name(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        viewHolder.btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(v.getContext(), "Clicked on Information ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        viewHolder.Share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(view.getContext(), "Clicked on Share ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        viewHolder.Edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(view.getContext(), "Clicked on Edit  ", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        viewHolder.Delete.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                mItemManger.removeShownLayouts(viewHolder.swipeLayout);
+//                studentList.remove(position);
+//                notifyItemRemoved(position);
+//                notifyItemRangeChanged(position, studentList.size());
+//                mItemManger.closeAllItems();
+//                Toast.makeText(v.getContext(), "Deleted " + viewHolder.Name.getText().toString(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        mItemManger.bindView(viewHolder.itemView, position);
     }
 
     @Override
@@ -50,9 +140,22 @@ public class EventActivityRecyclerViewAdapter extends RecyclerView.Adapter<Event
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener {
         private TextView name, score, weight, status, target;
+        private SwipeLayout swipeLayout;
+        private ImageButton btnLocation;
+        private TextView Delete;
+        private TextView Edit;
+        private TextView Share;
+
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
+            swipeLayout = itemView.findViewById(R.id.list_row);
+            btnLocation = (ImageButton) itemView.findViewById(R.id.btnLocation);
+            Delete = (TextView) itemView.findViewById(R.id.Delete);
+            Edit = (TextView) itemView.findViewById(R.id.Edit);
+            Share = (TextView) itemView.findViewById(R.id.Share);
+
+
             name = itemView.findViewById(R.id.title);
             weight = itemView.findViewById(R.id.description1);
             score = itemView.findViewById(R.id.description2);

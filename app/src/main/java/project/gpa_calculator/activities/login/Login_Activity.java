cc73f2861b232b72baae80d795dbd37b195d0969
Huakun Shen <huakun.shen@mail.utoronto.ca>
2019-04-16 19:46:17 -0400

    add key hash generating to login activity

--- app/src/main/java/project/gpa_calculator/activities/login/Login_Activity.java
+++ app/src/main/java/project/gpa_calculator/activities/login/Login_Activity.java
@@ -1,9 +1,13 @@
 package project.gpa_calculator.activities.login;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -37,6 +41,8 @@ import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 
 import project.gpa_calculator.models.UserF;
@@ -71,6 +77,20 @@ public class Login_Activity extends AppCompatActivity implements View.OnClickLis
         // facebook setup
 //        FacebookSdk.sdkInitialize(getApplicationContext());
 //        AppEventsLogger.activateApp(this);
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    "project.gpa_calculator",
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
         initializeFacebookLoginBtn();
 
     }

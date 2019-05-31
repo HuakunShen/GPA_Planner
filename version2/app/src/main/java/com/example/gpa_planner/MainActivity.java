250ccb577a47b989464cd76bf163fe4e31a7bbb2
Huakun <shenhuakun@outlook.com>
2019-05-31 01:02:30 -0400

    created version 2 of the app, start over from beginning

---
+++ version2/app/src/main/java/com/example/gpa_planner/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.gpa_planner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+    }
+}
